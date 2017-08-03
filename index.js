46ced0815c067159c06a8b2a76505d47c1f84a42
Christopher Codrington <chcodrington@gmail.com>
2017-08-02 21:06:41 -0600

    wip, changing computers

--- index.js
+++ index.js
@@ -1,5 +1,43 @@
 var _ = require('lodash/fp'),
     faker = require('faker'),
-    fs = require('fs')
+    fs = require('fs'),
+    path = require('path')
 
-console.log('\nSo, I got this: ', process.argv[2])
+let fileArg = process.argv[2]
+
+let fileData = new Promise((resolve, reject) => {
+  fs.readFile(fileArg, 'utf8', (err, data) => {
+    if (err) reject(err)
+    return resolve(data)
+  })
+})
+
+// making assumptions about the shape of the data
+function expand(json) {
+  let shape = json["fkr/structure"]
+  return _.times(_.constant(json.structure), json["fkr/times"])
+}
+
+function makeFake(fakeVal) {
+  if (fakeVal.length == 1) {
+    return _.get(fakeVal[0], faker).call()
+  }
+  if (fakeVal.length > 1) {
+    let fkrArgs = _.slice(fakeVal, 1)
+    faker.apply(null, fkrArgs) 
+  }
+}
+
+function genFake(json) {
+  let myFn = (obj) => {
+    _.keys(objs)
+  }
+  _.map()
+}
+
+fileData.then(x => {
+  let data = JSON.parse(x)
+  let output = expand(data)
+  console.log('how many time?: ', output);
+})
+
