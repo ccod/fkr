d2e686d8f38c2c8b02b9729d0994adbfc09099f8
Christopher Codrington <chcodrington@gmail.com>
2017-08-02 22:41:38 -0600

    first simple cut, works, but not recursively yet.

--- index.js
+++ index.js
@@ -3,7 +3,18 @@ var _ = require('lodash/fp'),
     fs = require('fs'),
     path = require('path')
 
+// not really a fan of lodash/fp slice as far as I can tell,
+// because you can't default to end of array
+let args = _.slice(2, 8, process.argv)
 let fileArg = process.argv[2]
+
+// Super dumb error handler stub
+function error(err) { console.log('woops, err: ', err) }
+
+function maybeError(err) {
+  if (err) throw err
+  console.log('File successfully saved!')
+}
 
 let fileData = new Promise((resolve, reject) => {
   fs.readFile(fileArg, 'utf8', (err, data) => {
@@ -15,7 +26,7 @@ let fileData = new Promise((resolve, reject) => {
 // making assumptions about the shape of the data
 function expand(json) {
   let shape = json["fkr/structure"]
-  return _.times(_.constant(json.structure), json["fkr/times"])
+  return _.times(_.constant(json["fkr/structure"]), json["fkr/times"])
 }
 
 function makeFake(fakeVal) {
@@ -23,21 +34,20 @@ function makeFake(fakeVal) {
     return _.get(fakeVal[0], faker).call()
   }
   if (fakeVal.length > 1) {
-    let fkrArgs = _.slice(fakeVal, 1)
-    faker.apply(null, fkrArgs) 
+    let fkrArgs = _.slice(1, fakeVal.length - 1, fakeVal)
+    return _.get(fakeVal[0], faker).apply(null, fkrArgs)
   }
+  throw 'woops, not taking care of this case yet'
 }
 
 function genFake(json) {
-  let myFn = (obj) => {
-    _.keys(objs)
-  }
-  _.map()
+  return _.map(x => _.mapValues(val => makeFake(val))(x), json)
 }
 
 fileData.then(x => {
   let data = JSON.parse(x)
-  let output = expand(data)
-  console.log('how many time?: ', output);
-})
+  let output = _.compose(genFake, expand)(data)
+  if (args.length == 1) { return process.stdout.write(JSON.stringify(output)) }
+  if (args.length == 2) { return fs.writeFile(args[1], JSON.stringify(output), 'utf8', maybeError) }
+}).catch(error)
 
